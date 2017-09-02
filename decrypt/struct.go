ab6e3b81225e87853edfc2126f71e4cb08ecb824
Andrea Caforio <andrea.caforio@epfl.ch>
2017-09-02 21:10:19 +0200

    Initiate Decrypt Protocol
    Issue: Separate directory was missing.
    Change: Create said directory and add protocol message structures.

--- decrypt/struct.go
+++ decrypt/struct.go
@@ -1,19 +1,18 @@
-package shuffle
+package decrypt
 
 import (
-	"github.com/dedis/cothority/skipchain"
+	"github.com/qantik/nevv/api"
 
 	"gopkg.in/dedis/onet.v1"
 )
 
 // Name defines the protocol identifier in the onet service.
-const Name = "shuffle"
+const Name = "decrypt"
 
-// Prompt is the message sent from one node to another to invoke a new
-// shuffle procedure at the receiver.
+// Prompt is the message sent from one node to another to invoke a new decryption
+// of Box of ballots at the receiver.
 type Prompt struct {
-	Genesis *skipchain.SkipBlock
-	Latest  *skipchain.SkipBlock
+	Box *api.Box
 }
 
 // MessagePrompt wraps the Prompt message. For compatibilty reasons demanded
@@ -24,9 +23,9 @@ type MessagePrompt struct {
 }
 
 // Terminate is sent by the leaf node to the root node to signal that the last
-// shuffle has been appended to the SkipChain.
+// decryption of Box of shuffles has been perfomed.
 type Terminate struct {
-	Latest *skipchain.SkipBlock
+	Box *api.Box
 }
 
 // MessageTerminate wraps the Terminate message. For compatibility reasons
