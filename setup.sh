e7f2dd0837b048f504eafa23dfa2f7bafe32e62f
Andrea Caforio <andrea.caforio@epfl.ch>
2017-10-30 13:23:22 +0100

    setup.sh Lint
    Issue: Reduce eight-space indentation.
    Change: Make it four.

--- setup.sh
+++ setup.sh
@@ -5,73 +5,73 @@ GOPATH=${GOPATH:-`go env GOPATH`}
 CONODE_GO=github.com/qantik/nevv
 
 main() {
-	if [ ! "$GOPATH" ]; then
-		echo "'$GOPATH' not found"
-		exit 1
-	fi
-
-	if ! echo $PATH | grep -q "$GOPATH/bin"; then
-		PATH=$PATH:$GOPATH/bin
-	fi
-
-	if [ "$#" -lt 1 ]; then
-	    echo "Specify task"
-	    exit 1
-	fi
-
-	case $1 in
-	    run)
-		run $@ ;;
-	    stop)
-		stop ;;
-	    test)
-		test ;;
-	    clean)
-		clean ;;
-	    *)
-		echo "Task '$ACTION' not found." ;;
-	esac
+    if [ ! "$GOPATH" ]; then
+    	echo "'$GOPATH' not found"
+    	exit 1
+    fi
+    
+    if ! echo $PATH | grep -q "$GOPATH/bin"; then
+    	PATH=$PATH:$GOPATH/bin
+    fi
+    
+    if [ "$#" -lt 1 ]; then
+        echo "Specify task"
+        exit 1
+    fi
+    
+    case $1 in
+        run)
+    	    run $@ ;;
+        stop)
+    	    stop ;;
+        test)
+    	    test ;;
+        clean)
+    	    clean ;;
+        *)
+    	    echo "Task '$ACTION' not found." ;;
+    esac
 }
 
 run() {
-	if [ ! "$2" ]; then
-		echo "Specify number of nodes"
-		exit 1
-	fi
-
-	NODES=$2
-	DEBUG=1
-
-	if [ "$3" ]; then
-	    DEBUG=$3
-	fi
-
-	killall -9 $CONODE 2> /dev/null || true
-	go install $CONODE_GO
-	rm -f public.toml
-
-	for n in $( seq $NODES ); do
-		co=co$n
-		if [ -f $co/public.toml ]; then
-			if ! grep -q Description $co/public.toml; then
-				rm -rf $co
-			fi
-		fi
-
-		if [ ! -d $co ]; then
-			echo -e "127.0.0.1:$((7000 + 2 * $n))\nConode_$n\n$co" | $CONODE setup
-		fi
-
-		$CONODE -c $co/private.toml -d $DEBUG &
-		cat $co/public.toml >> public.toml
-	done
-
-	sleep 1
-	echo "Conodes setup successful. Use public.toml to interact with the cothority."
+    if [ ! "$2" ]; then
+    	echo "Specify number of nodes"
+    	exit 1
+    fi
+    
+    NODES=$2
+    DEBUG=1
+    
+    if [ "$3" ]; then
+        DEBUG=$3
+    fi
+    
+    killall -9 $CONODE 2> /dev/null || true
+    go install $CONODE_GO
+    rm -f public.toml
+    
+    for n in $( seq $NODES ); do
+    	co=co$n
+    	if [ -f $co/public.toml ]; then
+	    if ! grep -q Description $co/public.toml; then
+    		rm -rf $co
+	    fi
+    	fi
+    
+    	if [ ! -d $co ]; then
+    		echo -e "127.0.0.1:$((7000 + 2 * $n))\nConode_$n\n$co" | $CONODE setup
+    	fi
+    
+    	$CONODE -c $co/private.toml -d $DEBUG &
+    	cat $co/public.toml >> public.toml
+    done
+    
+    sleep 1
+    echo "Conodes setup successful. Use public.toml to interact with the cothority."
 }
 
 stop() {
-	killall -9 $CONODE 2> /dev/null || true
+    killall -9 $CONODE 2> /dev/null || true
 }
 
 test() {
