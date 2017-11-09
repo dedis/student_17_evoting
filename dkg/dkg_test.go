e14ce971e46f9f98e18025a506df6e35f9e88efb
Andrea Caforio <andrea.caforio@epfl.ch>
2017-11-09 15:35:18 +0100

    Rename DKG Test Function
    Issue: The name setupDKG had no equivalent in the package.
    Change: Rename to generations.

--- dkg/dkg_test.go
+++ dkg/dkg_test.go
@@ -17,22 +17,23 @@ func TestMain(m *testing.M) {
 
 func TestProtocol(t *testing.T) {
 	for _, nodes := range []int{3, 5, 10} {
-		setupDKG(t, nodes)
+		protocol(t, nodes)
 	}
 }
 
-func setupDKG(t *testing.T, nbrNodes int) {
+func protocol(t *testing.T, nodes int) {
 	local := onet.NewLocalTest()
 	defer local.CloseAll()
 
-	_, _, tree := local.GenBigTree(nbrNodes, nbrNodes, nbrNodes, true)
+	_, _, tree := local.GenBigTree(nodes, nodes, nodes, true)
 
 	pi, err := local.CreateProtocol(Name, tree)
-	protocol := pi.(*Protocol)
-	protocol.Wait = true
 	if err != nil {
 		t.Fatal("Couldn't start protocol:", err)
 	}
+
+	protocol := pi.(*Protocol)
+	protocol.Wait = true
 	log.ErrFatal(pi.Start())
 
 	timeout := network.WaitRetry * 2 * time.Second
