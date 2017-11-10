3f07605d0332b7cb08cf077f1a2f62d79f80e092
Andrea Caforio <andrea.caforio@epfl.ch>
2017-11-10 23:31:46 +0100

    State Scheduler
    Issue: Logged users were not removed when time limit was reached.
    Change: Introduce a scheduler function.

--- service/state.go
+++ service/state.go
@@ -5,8 +5,44 @@ import (
 	"time"
 )
 
+type user struct {
+	sciper int
+	admin  bool
+	time   int
+}
+
+type state struct {
+	log map[string]*user
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
+}
+
+// schedule periodically increments the time counter for each user in the
+// state log and removes him if the time limit has been reached.
+func (s *state) schedule(interval time.Duration) chan bool {
+	ticker := time.NewTicker(interval)
+	stop := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				for nonce, user := range s.log {
+					if user.time == 5 {
+						delete(s.log, nonce)
+					} else {
+						user.time++
+					}
+				}
+			case <-stop:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	return stop
 }
 
 // nonce returns a random string for a given length n.
--- service/state_test.go
+++ service/state_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -10,4 +11,22 @@ func TestNonce(t *testing.T) {
 	n1, n2, n3 := nonce(10), nonce(10), nonce(10)
 	assert.Equal(t, 10, len(n1), len(n2), len(n3))
 	assert.NotEqual(t, n1, n2, n3)
+}
+
+func TestSchedule(t *testing.T) {
+	s := state{make(map[string]*user)}
+	s.log["u1"] = &user{0, false, 0}
+	s.log["u2"] = &user{1, false, 2}
+	s.log["u3"] = &user{2, false, 4}
+
+	stop := s.schedule(time.Second)
+	<-time.After(1000 * time.Millisecond)
+	assert.Equal(t, 3, len(s.log))
+	<-time.After(1000 * time.Millisecond)
+	assert.Equal(t, 2, len(s.log))
+	<-time.After(2000 * time.Millisecond)
+	assert.Equal(t, 1, len(s.log))
+	<-time.After(2000 * time.Millisecond)
+	assert.Equal(t, 0, len(s.log))
+	stop <- true
 }
