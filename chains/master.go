b80466e8433425f797e3b93ad0add84ab431f9e7
Andrea Caforio <andrea.caforio@epfl.ch>
2017-12-17 14:18:34 +0100

    Master Documentation
    Issue: master.go had no comments whatsoever.
    Change: Add documentation to structures and functions.

--- chains/master.go
+++ chains/master.go
@@ -4,7 +4,6 @@ import (
 	"gopkg.in/dedis/cothority.v1/skipchain"
 	"gopkg.in/dedis/crypto.v0/abstract"
 	"gopkg.in/dedis/onet.v1"
-	"gopkg.in/dedis/onet.v1/log"
 	"gopkg.in/dedis/onet.v1/network"
 )
 
@@ -13,20 +12,29 @@ func init() {
 	network.RegisterMessage(&Link{})
 }
 
+// Master is the foundation object of the entire service.
+// It contains mission critical information that can only be
+// set by an administrator that has access the conodes.
 type Master struct {
-	Key    abstract.Point
+	// Key is the front-end public for authenticity control.
+	Key abstract.Point
+	// Roster is a list of conodes handling the service.
 	Roster *onet.Roster
+	// Admins is list of users that can execute priviledged instructions.
 	Admins []User
 }
 
+// Link is a wrapper around the genesis Skipblock identifier of an
+// election. Every newly created election adds a new link to the
+// master Skipchain.
 type Link struct {
 	Genesis skipchain.SkipBlockID
 }
 
+// IsAdmin checks if a given user is part of the administrator list
+// of the master Skipchain.
 func (m *Master) IsAdmin(user User) bool {
-	log.Lvl3(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", m.Admins)
 	for _, admin := range m.Admins {
-		log.Lvl3("******", admin, user, admin == user)
 		if admin == user {
 			return true
 		}
