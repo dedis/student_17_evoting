1f007be053aab88f655b835eb396aae05df0c762
Andrea Caforio <andrea.caforio@epfl.ch>
2017-12-22 12:56:15 +0100

    Move Shuffle to elgamal.go
    Issue: Shuffle function was too hidden in shuffle package.
    Change: Move it to the ElGamal procedures in the crypto package.

--- crypto/elgamal.go
+++ crypto/elgamal.go
@@ -1,23 +1,80 @@
 package crypto
 
-import "gopkg.in/dedis/crypto.v0/abstract"
+import (
+	"errors"
+
+	"gopkg.in/dedis/crypto.v0/abstract"
+	"gopkg.in/dedis/crypto.v0/proof"
+	"gopkg.in/dedis/crypto.v0/random"
+	"gopkg.in/dedis/crypto.v0/shuffle"
+)
 
 // Encrypt performs the standard ElGamal encryption algorithm.
-// (K, C) = (g^k, public^k * m).
+// (alpha, beta) = (g^k, public^k * m).
 func Encrypt(public abstract.Point, message []byte) (abstract.Point, abstract.Point) {
 	M, _ := Suite.Point().Pick(message, Stream)
 
 	k := Suite.Scalar().Pick(Stream)
-	K := Suite.Point().Mul(nil, k)
+	alpha := Suite.Point().Mul(nil, k)
 	S := Suite.Point().Mul(public, k)
-	C := S.Add(S, M)
-	return K, C
+	beta := S.Add(S, M)
+	return alpha, beta
 }
 
 // Decrypt performs the standard ElGamal decryption algorithm.
-// m = C / (K^secret).
-func Decrypt(secret abstract.Scalar, K, C abstract.Point) ([]byte, error) {
-	S := Suite.Point().Mul(K, secret)
-	M := Suite.Point().Sub(C, S)
+// m = beta / (alpha^secret).
+func Decrypt(secret abstract.Scalar, alpha, beta abstract.Point) ([]byte, error) {
+	S := Suite.Point().Mul(alpha, secret)
+	M := Suite.Point().Sub(beta, S)
 	return M.Data()
+}
+
+// Shuffle permutes and reencrypts ElGamal ciphertext pairs and returns it with
+// prover function for verifiability.
+func Shuffle(public abstract.Point, alpha, beta []abstract.Point) (
+	[]abstract.Point, []abstract.Point, []int, proof.Prover, error) {
+
+	k := len(alpha)
+
+	if k != len(beta) {
+		return nil, nil, nil, nil, errors.New("ElGamal pair lists not of equal length")
+	}
+	if k < 2 {
+		return nil, nil, nil, nil, errors.New("Not enough elements (> 1) to permute")
+	}
+
+	ps := shuffle.PairShuffle{}
+	ps.Init(Suite, k)
+
+	pi := make([]int, k)
+	for i := 0; i < k; i++ {
+		pi[i] = i
+	}
+
+	for i := k - 1; i > 0; i-- {
+		j := int(random.Uint64(Stream) % uint64(i+1))
+		if j != i {
+			t := pi[j]
+			pi[j] = pi[i]
+			pi[i] = t
+		}
+	}
+
+	tau := make([]abstract.Scalar, k)
+	for i := 0; i < k; i++ {
+		tau[i] = Suite.Scalar().Pick(Stream)
+	}
+
+	gamma, delta := make([]abstract.Point, k), make([]abstract.Point, k)
+	for i := 0; i < k; i++ {
+		gamma[i] = Suite.Point().Mul(nil, tau[pi[i]])
+		gamma[i].Add(gamma[i], alpha[pi[i]])
+		delta[i] = Suite.Point().Mul(public, tau[pi[i]])
+		delta[i].Add(delta[i], beta[pi[i]])
+	}
+
+	prover := func(ctx proof.ProverContext) error {
+		return ps.Prove(pi, nil, public, tau, alpha, beta, Stream, ctx)
+	}
+	return gamma, delta, pi, prover, nil
 }
--- crypto/elgamal_test.go
+++ crypto/elgamal_test.go
@@ -3,6 +3,10 @@ package crypto
 import (
 	"testing"
 
+	"gopkg.in/dedis/crypto.v0/abstract"
+	"gopkg.in/dedis/crypto.v0/proof"
+	"gopkg.in/dedis/crypto.v0/shuffle"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,4 +18,35 @@ func TestElGamal(t *testing.T) {
 	K, C := Encrypt(public, message)
 	dec, _ := Decrypt(secret, K, C)
 	assert.Equal(t, message, dec)
+}
+
+func TestShuffle(t *testing.T) {
+	secret := Suite.Scalar().Pick(Stream)
+	public := Suite.Point().Mul(nil, secret)
+	message := []byte("nevv")
+
+	_, _, _, _, err := Shuffle(public, []abstract.Point{}, []abstract.Point{})
+	assert.NotNil(t, err)
+	_, _, _, _, err = Shuffle(public, []abstract.Point{Suite.Point()}, []abstract.Point{})
+	assert.NotNil(t, err)
+
+	n := 100
+
+	alpha, beta := make([]abstract.Point, n), make([]abstract.Point, n)
+	for i := 0; i < n; i++ {
+		alpha[i], beta[i] = Encrypt(public, message)
+	}
+
+	gamma, delta, _, prover, _ := Shuffle(public, alpha, beta)
+	prove, err := proof.HashProve(Suite, "", Stream, prover)
+	assert.Nil(t, err)
+
+	verifier := shuffle.Verifier(Suite, nil, public, alpha, beta, gamma, delta)
+	err = proof.HashVerify(Suite, "", verifier, prove)
+	assert.Nil(t, err)
+
+	for i := 0; i < n; i++ {
+		dec, _ := Decrypt(secret, gamma[i], delta[i])
+		assert.Equal(t, message, dec)
+	}
 }
